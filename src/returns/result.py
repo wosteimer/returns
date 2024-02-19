79918d3d6daa2bd641dd8d504c86ddf183e94462
William <wosteimer1@gmail.com>
2024-02-19 12:14:22 -0300

    Revert "refactor: change generics names"
    This reverts commit e1afc897291b9ae2a33afd7d8e278799ae0a3ff3.

--- src/returns/result.py
+++ src/returns/result.py
@@ -1,11 +1,10 @@
 from __future__ import annotations
 
-from collections.abc import Awaitable, Callable
+from collections.abc import Callable
 
 from returns.errors import InvalidUnwrapError
 
 type Result[T, E: Exception] = Ok[T, E] | Err[T, E]
-
 
 
 class Ok[T, E: Exception]:
@@ -18,17 +17,14 @@ class Ok[T, E: Exception]:
     def value(self) -> T:
         return self.__value
 
-    async def async_map[U, R:Exception](self, fn: Callable[[T], Awaitable[Result[U, R]]]) -> Result[U, R]:
-        return await fn(self.__value)
-
     def map[U, R: Exception](self, fn: Callable[[T], Result[U, R]]) -> Result[U, R]:
         return fn(self.__value)
 
-    def unwrap(self) -> T:
-        return self.value
-    
     def unwrap_err(self) -> E:
         raise InvalidUnwrapError()
+
+    def unwrap(self) -> T:
+        return self.value
 
     def is_ok(self) -> bool:
         return True
@@ -46,9 +42,6 @@ class Err[T, E:Exception]:
     @property
     def value(self) -> E:
         return self.__value
-
-    async def async_map[U, R:Exception](self, _: Callable[[T], Awaitable[Result[U, R]]]) -> Result[U, E]:
-        return Err(self.__value) 
 
     def map[U, R: Exception](self, _: Callable[[T], Result[U, R]]) -> Result[U, E]: 
         return Err(self.__value)
